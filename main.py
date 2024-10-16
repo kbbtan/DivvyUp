0e298174b30741524c8ba929cf354b6a2800386c
Kian Boon <kbbbtan@gmail.com>
2024-10-15 18:24:49 -0700

    refactor: changed app to bot

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from pyrogram import Client, filters
 
-app = Client("my_bot")
+bot = Client("my_bot")
 
-@app.on_message(filters.text & filters.private)
+@bot.on_message(filters.text & filters.private)
 async def echo(client, message):
     await message.reply(message.text)
 
 # Automatically start() and idle()
 print("Starting Bot")
-app.run()
+bot.run()
